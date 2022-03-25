c12fddfc0736b64fcdb885264ffc795beb674dec
Jayesh Badwaik (FZ Juelich) <j.badwaik@fz-juelich.de>
2022-03-25 11:07:13 +0100

    + noninteractive install

--- install-llvm.sh
+++ install-llvm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DEBIAN_FRONTEND=noninteractive apt-get update
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
+DEBIAN_FRONTENT=noninteractive && apt-get -y install build-essential
+DEBIAN_FRONTENT=noninteractive && apt-get -y install software-properties-common
 apt install wget lsb-release software-properties-common
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
